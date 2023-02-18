fcff1074826b87c1eddb7dced4e4712709bee6d7
Bryanwebprogrammer2012 <119443381+Bryanwebprogrammer2012@users.noreply.github.com>
2023-02-18 21:34:29 +0300

    Update assignment#9 Class methods.py

--- assignment#9 Class methods.py
+++ assignment#9 Class methods.py
@@ -1,23 +1,18 @@
 class  rectangle:
-    def __init__(self, area_length, area_width, perimeter_length,perimeter_width):
-        self.area_length = area_length
-        self.area_width = area_width
-        self.perimeter_length = perimeter_length
-        self.perimeter_width = perimeter_width
-
+    def __init__(self, length, width ):
+        self.length = length
+        self.width = width
+# area calculation
     def area(self):
-        area = self.area_length*self.area_width
+        area = self.length*self.width
         print("\nThe area is ",area)
     def perimeter(self):
-        perimeter = perimeter_width+perimeter_length
+        perimeter = self.width + self.length
         print("\nThe perimeter is ",perimeter)
-    # area calculation
-area_length = int(input("\nEnter area (Length): "))
-area_width = int(input("\nEnter area (Width): "))
-# perimeter calcullation
-perimeter_length = int(input("\nEnter perimeter (Length): "))
-perimeter_width = int(input("\nEnter width (Width): "))
+
+length = int(input("\nEnter  Length: "))
+width = int(input("\nEnter  Width: "))
 # conclusion
-conclusion = rectangle(area_length,area_width,perimeter_length,perimeter_width)
-conclusion.area()
-conclusion.perimeter()
+a = rectangle(length,width)
+a.area()
+a.perimeter()
