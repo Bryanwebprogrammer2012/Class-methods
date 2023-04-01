32484b461e14f018af8a3c2bbdbace8bca8cf052
Bryanwebprogrammer2012 <119443381+Bryanwebprogrammer2012@users.noreply.github.com>
2023-04-01 21:32:31 +0300

    Add files via upload

--- Asignment_Payrol_System.py
+++ Asignment_Payrol_System.py
@@ -6,39 +6,45 @@ class Employee:
         self.name = name
 
 class SalaryEmployee(Employee):
-    def __init__(self, id, name):
+    def __init__(self, id, name,salary):
         super().__init__(id, name)
+        self.salary = salary
        
     def calculate_payroll(self):
-        weekly_salary = 20000
-        print("\nSalary employee's name : ",self.name,"\n Id : ",self.id,"\n Weekly salary: ",weekly_salary)
+        print("\nSalary employee's name : ",self.name,"\n Id : ",self.id,"\n Weekly salary: ",self.salary)
        
 class CommissionEmployee(SalaryEmployee):
-    def __init__(self, id, name):
-        super().__init__(id, name)
+    def __init__(self, id, name,salary,commission,):
+        super().__init__(id, name,salary)
+        self.commission = commission
       
 
     def calculate_payroll(self):
-        commission = 34444
-        print("\nCommission employee's name : ",self.name,"\n Id : ",self.id,"\n Weekly salary: ",commission)
+        formula = self.salary + self.commission
+        print("\nCommission employee's name : ",self.name,"\n Id : ",self.id,"\n Weekly salary: ",self.salary,"\nCommission: ",self.commission,"\nTotal: ",formula)
 
 class HourlyEmpoyee(Employee):
-    def __init__(self, id, name):
+    def __init__(self, id, name,hour_rate,hours_worked):
         super().__init__(id, name)
-       
-
+        self.hour_rate = hour_rate
+        self.hours_worked = hours_worked
     def calculate_payroll(self):
-        hour = 5
-        hour_rate = hour * 5000
-        print("\nHourly employee's name : ",self.name,"\n Id : ",self.id,"\n Weekly salary: ",hour_rate)
+        salary = self.hour_rate * self.hours_worked
+        print("\nHourly employee's name : ",self.name,"\n Id : ",self.id,"\n hours worked:  ",self.hours_worked,"\nHour rate : ",self.hour_rate,"\nSalary: ",salary)
 
+
+salary = int(input("\nEnter salary for salary employee: "))
 e = Employee(1223,"David")
-s = SalaryEmployee(1224,"John")
+s = SalaryEmployee(1224,"John",salary)
 s.calculate_payroll()
 
-c = CommissionEmployee(1233,"Bryan")
+salary_commission = int(input("\nEnter salary for commission employee: "))
+commission = int(input("\nEnter the commission for commission employee: "))
+c = CommissionEmployee(1233,"Bryan",salary_commission,commission)
 c.calculate_payroll() 
 
-h = HourlyEmpoyee(1244,"Carol")
+hour_rate = int(input("\nEnter the hour rate for hourly employee: "))
+hours_worked = int(input("\nEnter hours worked for hourly employee: "))
+h = HourlyEmpoyee(1244,"Carol",hour_rate,hours_worked)
 h.calculate_payroll()
 
